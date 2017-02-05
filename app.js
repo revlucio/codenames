5bed145e9aeb484af7cd670990657d4ea024542d
Luke Elliott <revlucio@gmail.com>
2017-02-05 11:14:07 +0000

    added a basic chat app

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-var express = require('express')
-var app = express()
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
